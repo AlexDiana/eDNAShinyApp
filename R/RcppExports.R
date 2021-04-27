b73c8d8da61396c88339693ba62007969bcaf252
alexdiana1992 <ad625@kent.ac.uk>
2021-04-27 22:59:57 +0200

    Fixing image issue again

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,30 +2,30 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 rpg <- function(n, z) {
-    .Call('_eDNAShinyApp_rpg', PACKAGE = 'eDNAShinyApp', n, z)
+    .Call(`_eDNAShinyApp_rpg`, n, z)
 }
 
 diagMatrixProd <- function(X, D) {
-    .Call('_eDNAShinyApp_diagMatrixProd', PACKAGE = 'eDNAShinyApp', X, D)
+    .Call(`_eDNAShinyApp_diagMatrixProd`, X, D)
 }
 
 sample_beta_cpp <- function(X, B, b, Omega, k) {
-    .Call('_eDNAShinyApp_sample_beta_cpp', PACKAGE = 'eDNAShinyApp', X, B, b, Omega, k)
+    .Call(`_eDNAShinyApp_sample_beta_cpp`, X, B, b, Omega, k)
 }
 
 sample_Omega_cpp <- function(X, beta, n) {
-    .Call('_eDNAShinyApp_sample_Omega_cpp', PACKAGE = 'eDNAShinyApp', X, beta, n)
+    .Call(`_eDNAShinyApp_sample_Omega_cpp`, X, beta, n)
 }
 
 sample_beta_nocov_cpp <- function(beta, X, b, B, n, k) {
-    .Call('_eDNAShinyApp_sample_beta_nocov_cpp', PACKAGE = 'eDNAShinyApp', beta, X, b, B, n, k)
+    .Call(`_eDNAShinyApp_sample_beta_nocov_cpp`, beta, X, b, B, n, k)
 }
 
 sample_gamma_beta_cpp <- function(gamma, beta, X, b, B, D, n, k, indexes_covariates, fixedIndexes, d_bar) {
-    .Call('_eDNAShinyApp_sample_gamma_beta_cpp', PACKAGE = 'eDNAShinyApp', gamma, beta, X, b, B, D, n, k, indexes_covariates, fixedIndexes, d_bar)
+    .Call(`_eDNAShinyApp_sample_gamma_beta_cpp`, gamma, beta, X, b, B, D, n, k, indexes_covariates, fixedIndexes, d_bar)
 }
 
 sample_z_cpp <- function(psi, p, k_s) {
-    .Call('_eDNAShinyApp_sample_z_cpp', PACKAGE = 'eDNAShinyApp', psi, p, k_s)
+    .Call(`_eDNAShinyApp_sample_z_cpp`, psi, p, k_s)
 }
 
